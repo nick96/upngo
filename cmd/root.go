fdbf51fb2b8a2eeefcfe26abb300b56d035e1812
Nick Spain <nicholas.spain@stileeducation.com>
2020-08-09 21:30:47 +1000

    Allow specifying verbosity

--- cmd/root.go
+++ cmd/root.go
@@ -2,15 +2,14 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var verbose *bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -28,31 +27,10 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
+	verbose = rootCmd.PersistentFlags().BoolP("verbose", "v", false, "Verbose logging")
+	cobra.OnInitialize(func() {
+		if !*verbose {
+			log.SetOutput(ioutil.Discard)
 		}
-
-		// Search config in home directory with name ".upngo" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".upngo")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	})
 }
